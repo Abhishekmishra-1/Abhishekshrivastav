356dcf9cddf1762950c4b0982bbb11529e4b0ee1
Abhishekmishra-1 <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-01-03 15:11:11 +0530

    Add files via upload

---
+++ allPrimeFactor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int x,i;
+    printf("Enter a number");
+    scanf("%d",&x);
+    for(i=2;x>1;i++)
+    {
+        while(x%i==0)
+        {
+            printf("%d ",i);
+            x=x/i;
+        }
+    }
+}
